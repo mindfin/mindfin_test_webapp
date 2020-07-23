58c3b6a5b4a74bf091f4d149266a54bc662f3905
Guru Charan <gururk2@gmail.com>
2020-07-23 14:18:00 +0000

    Update updateAndRestart.sh

--- deploy/updateAndRestart.sh
+++ deploy/updateAndRestart.sh
@@ -6,11 +6,11 @@ set -e
 
 echo "remove repo"
 # Delete the old repo
-rm -rf /home/mindfinadmin/mindfin
+rm -rf ~/mindfin/mindfin_test_webapp
 
 echo "clone repo"
 # clone the repo again
-git clone git@gitlab.com:mindfin1/mindfin.git /home/mindfinadmin/mindfin 
+git clone git@gitlab.com:mindfin1/mindfin.git ~/mindfin/
 
 
 
@@ -21,16 +21,16 @@ git clone git@gitlab.com:mindfin1/mindfin.git /home/mindfinadmin/mindfin
 
 # stop the previous pm2
 echo "pm2 stop mindfin-webapp"
-/home/mindfinadmin/.nvm/versions/node/v10.13.0/bin/pm2 stop mindfin-webapp
+pm2 stop mindfin_test
 
 echo "changeing to mindfin"
-cd /home/mindfinadmin/mindfin
+cd ~/mindfin/mindfin_test_webapp
 
 #install npm packages
 echo "Running npm install"
-/home/mindfinadmin/.nvm/versions/node/v10.13.0/bin/npm install
+npm install
 
 
 echo "pm2 start"
 #Restart the node server
-/home/mindfinadmin/.nvm/versions/node/v10.13.0/bin/pm2 start mindfin-webapp
+pm2 start mindfin_test
